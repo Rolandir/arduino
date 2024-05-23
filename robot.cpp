46b86c367c0ebab22b6f6dd0ada666116b35caa3
Rolandir <pkouyou@yahoo.fr>
2024-05-23 11:27:18 +0200

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -1,225 +1,143 @@
-#include <NewPing.h>
-#include "I2Cdev.h"
-#include <PID_v1.h>
-#include "MPU6050_6Axis_MotionApps20.h"
-
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
-
-MPU6050 mpu;
-
-#define TRIGGER_PIN 3
-#define ECHO_PIN 13
-#define MAX_DISTANCE 200
-
-// NewPing setup of pins and maximum distance
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-/*********Tune these 4 values for your BOT*********/
-double setpoint= 177.9;
-double Kp = 21;
-double Kd = 1;
-double Ki = 60;
-/******End of values setting*********/
-
-double input, output;
-PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady()
-{
-    mpuInterrupt = true;
-}
-
+const int frontEchoPin = 7;
+const int frontTriggerPin = 6;
+const int leftEchoPin = 11;
+const int leftTriggerPin = 10;
+const int rightEchoPin = 9;
+const int rightTriggerPin = 8;
+const int motorL1 = 2;
+const int motorL2 = 3;
+const int motorR1 = 4;
+const int motorR2 = 5;
+volatile float maxFrontDistance = 25.00;
+volatile float frontDuration, frontDistanceCm, leftDuration, leftDistanceCm, rightDuration, rightDistanceCm;
+volatile float maxLeftDistance, maxRightDistance = 20.00;
 void setup() {
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-  Wire.begin();
-  TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-  Fastwire::setup(400, true);
-  #endif
-
-  Serial.begin (9600); // sonar SR04  
-  Serial.begin(115200); // MPU6050
-
-  // initialize device
-    Serial.println(F("Initializing I2C devices..."));
-    mpu.initialize();
-
-     // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // load and configure the DMP
-    devStatus = mpu.dmpInitialize();
-
-    
-    // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1688); 
-
-      // make sure it worked (returns 0 if so)
-    if (devStatus == 0)
-    {
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
-
-        // enable Arduino interrupt detection
-        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-        
-        //setup PID
-        pid.SetMode(AUTOMATIC);
-        pid.SetSampleTime(10);
-        pid.SetOutputLimits(-255, 255);  
-    }
-    else
-    {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
-
-//Initialise the Motor outpu pins
-    pinMode (6, OUTPUT);
-    pinMode (9, OUTPUT);
-    pinMode (10, OUTPUT);
-    pinMode (11, OUTPUT);
-
-//By default turn off both the motors
-    analogWrite(6,LOW);
-    analogWrite(9,LOW);
-    analogWrite(10,LOW);
-    analogWrite(11,LOW);
+  // serial
+  Serial.begin(9600);
+  // ultrasonic
+  pinMode(frontTriggerPin, OUTPUT);
+  pinMode(frontEchoPin, INPUT);
+  pinMode(leftTriggerPin, OUTPUT);
+  pinMode(leftEchoPin, INPUT);
+  pinMode(rightTriggerPin, OUTPUT);
+  pinMode(rightEchoPin, INPUT);
+  // motors
+  pinMode(motorL1, OUTPUT);
+  pinMode(motorL2, OUTPUT);
+  pinMode(motorR1, OUTPUT);
+  pinMode(motorR2, OUTPUT);
 }
-
 void loop() {
-  
- 
-          
-    // if programming failed, don't try to do anything
-    if (!dmpReady) return;
-
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize)
-    {
-        //no mpu data - performing PID calculations and output to motors     
-        pid.Compute();   
-        
-        //Print the value of Input and Output on serial monitor to check how it is working.
-        Serial.print(input); Serial.print(" =>"); Serial.println(output);
-               
-        if (input>150 && input<200){//If the Bot is falling
-             
-            if (output>0){ //Falling towards front
-              Forward(); //Rotate the wheels forward
-              
-              }
-            else if (output<0){ //Falling towards back
-              Reverse(); //Rotate the wheels backward
-              }
-            }
-        else //If Bot not falling
-        Stop(); //Hold the wheels still
-        }
-
-    // reset interrupt flag and get INT_STATUS byte
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024)
-    {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  // front distance check
+  checkFrontDistance();
+  if (frontDistanceCm < maxFrontDistance) {
+    Serial.println("Too close");
+    checkLeftDistance();
+    delay(20);
+    checkRightDistance();
+    delay(20);
+    if (leftDistanceCm < rightDistanceCm)
+      moveRight();
+    else if (leftDistanceCm > rightDistanceCm) {
+      moveLeft();
     }
-    else if (mpuIntStatus & 0x02)
-    {
-        // wait for correct available data length, should be a VERY short wait
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-        // read a packet from FIFO
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize;
-
-        mpu.dmpGetQuaternion(&q, fifoBuffer); //get value for q
-        mpu.dmpGetGravity(&gravity, &q); //get value for gravity
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity); //get value for ypr
-
-        input = ypr[1] * 180/M_PI + 180;
-
-   }
+  }
+  else {
+    Serial.println("OK");
+    moveForward();
+  }
+  // left distance check
+  checkLeftDistance();
+  if (leftDistanceCm < maxLeftDistance) {
+    Serial.println("Left too close");
+    delay(20);
+    checkLeftDistance();
+    delay(20);
+    checkRightDistance();
+    delay(20);
+    if (leftDistanceCm > rightDistanceCm)
+      moveForward();
+    else if (leftDistanceCm < rightDistanceCm) {
+      moveRight();
+    }
+  }
+  // right distance check
+  checkRightDistance();
+  if (rightDistanceCm < maxRightDistance) {
+    Serial.println("Right too close");
+    delay(20);
+    checkRightDistance();
+    delay(20);
+    checkLeftDistance();
+    delay(20);
+    if (rightDistanceCm > leftDistanceCm)
+      moveForward();
+    else if (rightDistanceCm < leftDistanceCm) {
+      moveLeft();
+    }
+  }
 }
-
-void Forward() //Code to rotate the wheel forward 
-{
-    analogWrite(6,output);
-    analogWrite(9,0);
-    analogWrite(10,output);
-    analogWrite(11,0);
-    Serial.print("F"); //Debugging information 
+void checkFrontDistance() {
+  digitalWrite(frontTriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+  delayMicroseconds(4);
+  digitalWrite(frontTriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+  delayMicroseconds(10);
+  digitalWrite(frontTriggerPin, LOW);
+  frontDuration = pulseIn(frontEchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+  frontDistanceCm = frontDuration * 10 / 292 / 2;  //convertimos a distancia, en cm
+  Serial.print("Distance: ");
+  Serial.print(frontDistanceCm);
+  Serial.println(" cm");
 }
-
-void Reverse() //Code to rotate the wheel Backward  
-{
-    analogWrite(6,0);
-    analogWrite(9,output*-1);
-    analogWrite(10,0);
-    analogWrite(11,output*-1); 
-    Serial.print("R");
+void checkLeftDistance() {
+  digitalWrite(leftTriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+  delayMicroseconds(4);
+  digitalWrite(leftTriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+  delayMicroseconds(10);
+  digitalWrite(leftTriggerPin, LOW);
+  leftDuration = pulseIn(leftEchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+  leftDistanceCm = leftDuration * 10 / 292 / 2;  //convertimos a distancia, en cm
+  Serial.print("Left distance: ");
+  Serial.print(leftDistanceCm);
+  Serial.println(" cm");
 }
-
-void Stop() //Code to stop both the wheels
-{
-    analogWrite(6,0);
-    analogWrite(9,0);
-    analogWrite(10,0);
-    analogWrite(11,0); 
-    Serial.print("S");
+void checkRightDistance() {
+  digitalWrite(rightTriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+  delayMicroseconds(4);
+  digitalWrite(rightTriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+  delayMicroseconds(10);
+  digitalWrite(rightTriggerPin, LOW);
+  rightDuration = pulseIn(rightEchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+  rightDistanceCm = rightDuration * 10 / 292 / 2;  //convertimos a distancia, en cm
+  Serial.print("Right distance: ");
+  Serial.print(rightDistanceCm);
+  Serial.println(" cm");
 }
-
-void Loop_sr04(){
-   //delay(40);
-   unsigned int distance = sonar.ping_cm();
-   Serial.print(distance);
-   Serial.println("cm");
+void moveBackward() {
+  Serial.println("Backward.");
+  digitalWrite(motorL1, HIGH);
+  digitalWrite(motorL2, LOW);
+  digitalWrite(motorR1, HIGH);
+  digitalWrite(motorR2, LOW);
+}
+void moveForward() {
+  Serial.println("Forward.");
+  digitalWrite(motorL1, LOW);
+  digitalWrite(motorL2, HIGH);
+  digitalWrite(motorR1, LOW);
+  digitalWrite(motorR2, HIGH);
+}
+void moveLeft() {
+  Serial.println("Left.");
+  digitalWrite(motorL1, LOW);
+  digitalWrite(motorL2, HIGH);
+  digitalWrite(motorR1, HIGH);
+  digitalWrite(motorR2, LOW);
+}
+void moveRight() {
+  Serial.println("Right.");
+  digitalWrite(motorL1, HIGH);
+  digitalWrite(motorL2, LOW);
+  digitalWrite(motorR1, LOW);
+  digitalWrite(motorR2, HIGH);
 }
